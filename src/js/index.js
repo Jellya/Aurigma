bde71d2e40e37b7a91ddfa0e104b2a62e2ec8ca2
Jellya <koipression@gmail.com>
2020-11-30 14:58:42 +0700

    js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,27 @@
+import jQuery from "jquery";
+
 var acc = document.getElementsByClassName("footer__title");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var answer = this.nextElementSibling;
-    if (answer.style.maxHeight) {
-        answer.style.maxHeight = null;
+    var links = this.nextElementSibling;
+    if (links.style.maxHeight) {
+      links.style.maxHeight = null;
     } else {
-        answer.style.maxHeight = answer.scrollHeight + "px";
+      links.style.maxHeight = links.scrollHeight + "px";
     } 
   });
 }
+
+function PopUpShow() {
+  jQuery("#burgermenu").show();
+}
+function PopUpHide() {
+  jQuery("#burgermenu").hide();
+}
+jQuery(document).ready(function () {
+  PopUpHide();
+});
+
